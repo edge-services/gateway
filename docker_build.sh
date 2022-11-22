42897a7672c1a178d8b15337a02cf992a762e9ac
Gurvinder Singh <gurvsin3@in.ibm.com>
2022-11-22 15:25:41 +0530

    small changes

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export APP_VERSION=$(grep -oE "\"version\": \"(\w*.\w*.\w*.\w*.\w*.)" app/package.json | cut -d\" -f4)
-export ARCH=arm32v6
+export ARCH=amd64
 export NODE_VERSION=12.22.1
-export OS=alpine
+export OS=buster-slim
 
 echo "#########################################################################"
 echo "Build EdgeGateway Service version: ${APP_VERSION} for ${ARCH}"
@@ -12,16 +12,16 @@ echo "#########################################################################"
 #   --push -t sinny777/security_arm64:0.0.1 \
 #   --platform=linux/arm64 .
 
-sudo docker buildx build --rm  \
+sudo docker buildx build --rm \
     --build-arg ARCH=${ARCH} \
     --build-arg VERSION=${APP_VERSION} \
     --build-arg NODE_VERSION=${NODE_VERSION} \
     --build-arg OS=${OS} \
     --build-arg TAG_SUFFIX=${TAG_SUFFIX} \
     --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
-    --file Dockerfile.alpine \
-    --tag sinny777/edge-gateway_arm:${APP_VERSION} \
-    --platform=linux/arm/v6,linux/arm/v7 --push .
+    --file Dockerfile \
+    --tag sinny777/edge-gateway_${ARCH}:${APP_VERSION} \
+    --platform=linux/amd64 --push .
 
 # sudo docker build --rm  \
 #     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
